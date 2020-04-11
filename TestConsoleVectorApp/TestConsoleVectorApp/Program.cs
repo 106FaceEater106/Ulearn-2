f14639983e4036171d46c5b37f737308bf7e97c9
Apolix96 <andreyevtukhov89@gmail.com>
2020-04-11 12:38:35 +0500

    add Practice Vector Main OOP

---
+++ TestConsoleVectorApp/TestConsoleVectorApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VectorTask;
+namespace TestConsoleVectorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vectorTest = new Vector() {X = 1, Y = 1 };
+            Console.WriteLine("Длина вектора: {0}", Geometry.GetLength(vectorTest));
+            Console.ReadLine();
+        }
+    }
+}
