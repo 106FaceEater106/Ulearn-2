1ed498ba94b2e6c61dcaafce9c7bd6f130d18710
Andrey Evtukhov <48060782+Apolix96@users.noreply.github.com>
2020-04-15 21:49:33 +0500

    CHAPTER 2

---
+++ Глава2/Rublez/Rublez/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rublez
+{
+    class Program
+    {
+        public static double Calculate(string splitRublez)
+        {
+            var rublez = double.Parse(splitRublez.Split(' ')[0]);
+            var percent = double.Parse(splitRublez.Split(' ')[1]) / 100;
+            var month = int.Parse(splitRublez.Split(' ')[2]);
+
+            for(var i = 1; i <= month; i++)
+            {
+                rublez += rublez * percent / 12;
+            }
+
+            return rublez;
+        }
+        static void Main(string[] args)
+        {
+            string splitRublez = Console.ReadLine();
+            var value = Calculate(splitRublez);
+            Console.WriteLine(value);
+            Console.ReadLine();
+
+        }
+    }
+}
