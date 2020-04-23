d0008fd9f0735ad25a3f10bc07ecdeb3f0ea9947
Andrey Evtukhov <48060782+Apolix96@users.noreply.github.com>
2020-04-23 16:39:15 +0500

    Add files via upload

---
+++ стеки и очереди часть 2/LimitedSizeStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        private int range; 
+        private int cout; 
+        private readonly T[] mass; 
+        private readonly int restriction;
+
+        public LimitedSizeStack(int limit)
+        {
+            restriction = limit; 
+            mass = new T[limit]; 
+            range = cout = 0;
+        }
+
+        public void Push(T item)
+        {
+            if (range - cout >= restriction) 
+            {
+                cout++; 
+            }
+            int residue = range % restriction;
+            mass[residue] = item; 
+            range++;
+        }
+
+        public T Pop()
+        {
+            if (range == cout)
+            {
+                return default(T);
+            } 
+            range--; 
+            int residue = range % restriction; 
+            return mass[residue];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return range - cout;
+            }
+        }
+    }
+}
